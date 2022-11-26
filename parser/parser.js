2aa7954147740668e4a70d6fadb45e08d84225d6
Andrew Boktor <andrew.boktor@gmail.com>
2022-11-26 12:49:21 -0800

    Full working civics app

---
+++ parser/parser.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+console.log('node ./parser/parser.js parser/100q.txt src/Questions.json');
+
+const text = fs.readFileSync(process.argv[2]).toString();
+
+const regex = /[0-9]+\. .+\r\n((?!\r\n\r\n\r\n)(\r\n(.*)))+/g
+
+const matches = text.match(regex);
+
+const baby = /([0-9]+\. .+)\r\n((\r\n.*)+)/
+
+const qna = matches.map(m => {
+    const full = m.match(baby);
+    return {Q: full[1].trim(), A: full[2].trim()};
+});
+
+fs.writeFileSync(process.argv[3], JSON.stringify(qna, null, 2));
+
+// var readlineSync = require('readline-sync');
+
+// const ask = (q) =>{
+//     readlineSync.question(q.Q);
+//     console.log(q.A);
+// }
+
+// while(qna.length) {
+//     const index = Math.floor(Math.random()*qna.length);
+//     const q = qna[index];
+//     ask(q);
+//     qna.splice(index, 1);
+//     console.log(qna.length, 'remaining');
+// }
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from 'react';  
+import './App.css';
+import questions from './Questions.json';
+
+interface Question {
+  Q: string,
+  A: string
+}
+
+const qs = questions as Question[];
+const randomize = (n: number) => {
+  const arr = new Array(n);
+  for(let i=0; i<n; i+=1) {
+    arr[i] = i;
+  }
+  const newarr = [];
+  while(arr.length) {
+    const i = Math.floor(Math.random()*arr.length);
+    newarr.push(arr[i]);
+    arr.splice(i,1);
+  }
+  return newarr;
+}
+
+function Text(s: string) {
+  return s.split('\r\n').map(piece => <><br/> {piece} </>)
+}
+function App() {
+  const [list, setList] = React.useState<number[]>(() => randomize(qs.length));
+  const currQ = list[list.length-1];
+  const [showA, setShowA] = React.useState(false);
+  const onNext = React.useCallback(() => {
+    // Pick a new Question;
+    list.pop();
+    setList([...list]);
+    setShowA(false);
+  }, [list]);
+  const onShowA = React.useCallback(() => {
+    setShowA(true);
+  }, []);
+
+  return (
+    <>
+      <h1>{Text(qs[currQ].Q)}</h1>
+      {showA && <h2>{Text(qs[currQ].A)}</h2>}
+      <button onClick={onShowA}>Show Answer</button>
+      <button onClick={onNext}>Next Question</button>
+    </>
+  );
+}
+
+export default App;
